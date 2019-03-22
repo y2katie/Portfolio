658e06467d6e532c763626030920eab5bb9d12b4
katyvonkaty <kj0203@gmail.com>
2019-03-22 17:20:06 -0400

    updated

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'No Kids' });
+  res.render('test', { title: 'AYO' });
 });
 
 module.exports = router;
