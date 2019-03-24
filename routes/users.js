41216c0ca204cd44e8409eb22dbf792ce0650902
“katyvonkaty” <kj0203@gmail.com>
2019-03-24 13:41:08 -0400

    updated

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users', { title: 'No Kids' });
+  res.render('users', { title: 'This is cool tho' });
 });
 
 module.exports = router;
