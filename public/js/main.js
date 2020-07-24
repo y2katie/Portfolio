5881f4fc00016e37552cbe5fb2ed8efb3e4efb00
“katyvonkaty” <kj0203@gmail.com>
2020-07-24 11:13:41 -0400

    updated

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-$('.typewriter').typeIt({
-     strings: ["Im a Graphic Designer.", "Scratch that - Im a Web Design and Developing Guru"],
-     speed: 50,
-     breakLines: false,
-     autoStart: false
-});
