a063485cd4e522944eb05730a0d203da1000071d
katyvonkaty <kj0203@gmail.com>
2020-09-02 16:50:28 -0400

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+app.use(express.static(__dirname + '../public'));
 
-module.exports = router;
+// /* GET home page. */
+ router.get('/', function(req, res) {
+   res.render('index', { title: 'Express' });
+ });
+
+ module.exports = router;
+
+
+
+// router.listen(process.env.PORT || 3000, function(){
+//   console.log('all systems go')
+// })
